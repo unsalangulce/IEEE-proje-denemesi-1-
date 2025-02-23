d47b8fb0cd6921bcba6bb4102706f76882e8b6b8
unsalangulce <gulce.unsalan@gmail.com>
2025-02-23 21:06:29 +0300

    Update clusterumap.py

--- clusterumap.py
+++ clusterumap.py
@@ -7,26 +7,19 @@ print(adata)
 print(adata.obs.head())
 print(adata.var.head())
 
-# Normalize et ve log-transform uygula
 sc.pp.normalize_total(adata, target_sum=1e4)
 sc.pp.log1p(adata)
 
-# Değişken genleri seç
 sc.pp.highly_variable_genes(adata, n_top_genes=2000)
 adata = adata[:, adata.var.highly_variable]
 
-# PCA ile boyut indirgeme
+
 sc.pp.pca(adata, n_comps=50)
-# UMAP için komşuluk grafiğini hesapla
 sc.pp.neighbors(adata, n_neighbours=15, n_pcs=30)
 
-# UMAP analizi
 sc.tl.umap(adata)
-
-# Clustering yap
 sc.tl.leiden(adata, resolution=0.5)
 
-# Sonuçları görselleştirme
 sc.pl.umap(adata, color=["leiden"])
 print(adata.obs)
 print(adata.obs["leiden"])
