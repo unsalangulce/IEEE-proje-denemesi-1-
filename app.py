9e7755cd1f900ede5f8076b3ebaeb485d813a809
unsalangulce <gulce.unsalan@gmail.com>
2025-03-13 22:45:52 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,65 @@
+import shiny 
+import scanpy as sc 
+import zipfile 
+import matplotlib 
+import matplotlib.pyplot 
+import os 
+import anndata
+import scipy 
+import time
+import pandas
+import numpy
+import os
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import seaborn as sns
+
+
+mpl.rcParams['figure.dpi'] = 70
+t0start = time.time()
+# plt.style.use('dark_background')
+
+
+from sklearn.decomposition import PCA
+#file upload butonu h5ad formatlı dosyaları kabul eder
+
+from shiny import App, Inputs, Outputs, Session, reactive, render,ui
+from zipfile import ZipFile
+
+from shiny.types import FileInfo
+ #codes needed for umap visualization importet as module
+#user interface 
+app_ui = ui.page_fluid(
+    ui.input_file("file1", "Choose File", accept=[".zip"], multiple=False),
+   
+    ui.output_table("summary"),
+    ui.h1("SingleCell UMAP Viewer in Shiny for Python"),
+    ui.output_image("output_image")
+)
+
+
+def server(input: Inputs, output: Outputs, session: Session):
+    @output
+    def unzip_file(zip_path, extract_to):
+        with zipfile.ZipFile(zip_path,'r') as zip_ref:
+            zip_ref.extractall(extract_to)    
+    @output
+    @reactive.calc
+    def parsed_file():
+        file: list[FileInfo] | None = input.file1()
+        try:
+            if file is None:
+                return pd.DataFrame()
+                return pd.read_h5ad( 
+                      file[0]["datapath"] )
+        except:
+            return f"file uploaded"
+    
+    @output
+    @render.table
+    def summary():
+        df = parsed_file()
+
+        if df.empty:
+            return pd.DataFrame()
+
--- functions.py
+++ functions.py
@@ -1,53 +1,49 @@
-import scanpy 
-import anndata
-import scipy 
-import time
-t0start = time.time()
-import pandas
-import numpy
+import zipfile
 import os
-import sys
+from shiny import App, ui, render
+import scanpy
 import matplotlib.pyplot as plt
-import matplotlib as mpl
-mpl.rcParams['figure.dpi'] = 70
-import seaborn as sns
-from sklearn.decomposition import PCA
+import tempfile
 
-file_path = 'Hw3covid_Data_AllCells.h5ad'
-if 1:  
-    t0 = time.time()
-    adata = scanpy.read(file_path)
-    print('%.1f'%(-t0+time.time()), ' seconds passed' )
-    print(type(adata.X))
-    adata
+def extractzip(zip_path):
+
+    extract_folder = "./data"
+
+    with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+        
+        zip_ref.extractall(extract_folder)
+        print(f"{zip_path} has extracted.") #unzipped the file
+
+    h5ad_file = ""
+    for file_name in os.listdir(extract_folder):
+        if file_name.endswith('.h5ad'):  
+            h5ad_file = os.path.join(extract_folder, file_name) #open .h5ad file
+            break
+        
+    if h5ad_file == "":
+        print(".h5ad file not found in the zip.")
     
-print(adata.obs['Cell type'].unique())
-print('Look at count matrix. We see integers - that confirms - data are raw-counts, not preprocessed expressions')
-print(adata.X.sum(), type(adata.X), numpy.asarray(adata.X.sum(axis = 1)).ravel()[:10])
-scanpy.pp.calculate_qc_metrics(adata,  percent_top=None, log1p=False, inplace=True)
-scanpy.pp.filter_cells(adata, min_genes=200) # Remove cells with more than 200 and less than 8000 detected genes
-scanpy.pp.filter_cells(adata, max_genes=8000) # Remove cells with more than 200 and less than 8000 detected genes
+    return h5ad_file
 
-scanpy.pp.filter_genes(adata, min_cells=3) # Remove genes detected in less than 3 cells
-adata
-#if 0: 
-#    # Remove cells with less than 5000 counts
-#    adata = adata[adata.obs.n_genes_by_counts <5000, :]    
-#    adata
+def umap_process(data):
+    adata = scanpy.read(data)
+    adata.raw = adata
     
-scanpy.pp.normalize_total(adata, target_sum=1e4) # normalize with counts per million
-scanpy.pp.log1p(adata) #take the log(1+x) of each value
+    scanpy.pp.calculate_qc_metrics(adata, percent_top=None, log1p=False, inplace=True)#QC metrics
+    scanpy.pp.filter_cells(adata, min_genes=200) # Remove cells with more than 200 and less than 8000 detected genes
+    scanpy.pp.filter_cells(adata, max_genes=8000) # Remove cells with more than 200 and less than 8000 detected genes
+    scanpy.pp.filter_genes(adata, min_cells=3) # Remove genes detected in less than 3 cells
+    scanpy.pp.normalize_total(adata, target_sum=1e4) # normalize with counts per million
+    scanpy.pp.log1p(adata)
+    scanpy.pp.highly_variable_genes(adata, min_mean=0.0125, max_mean=3, min_disp=0.5)
+    adata = adata[:, adata.var.highly_variable]
+    scanpy.pp.scale(adata, max_value=10) # subtract the mean expression value and divide by the standard deviation
+    scanpy.pp.neighbors(adata, n_neighbors=10, n_pcs=40)  # A neighborhood graph is computed for the data.
+    scanpy.tl.umap(adata)
+    scanpy.pl.umap(adata, show=False) #The UMAP embedding is plotted without displaying it.
 
-if 0:
-    # sc.pp.regress_out(adata, ['total_counts', 'pct_counts_mt'])
-    sc.pp.regress_out(adata, ['total_counts'])
+    temp_file = tempfile.NamedTemporaryFile(suffix=".png", delete=False)
+    plt.savefig(temp_file.name, bbox_inches='tight')  # PNG olarak kaydet
+    plt.close()
+    return temp_file.name
 
-scanpy.pp.highly_variable_genes(adata, min_mean=0.0125, max_mean=3, min_disp=0.5)
-
-adata = adata[:, adata.var.highly_variable]
-
-scanpy.pp.scale(adata, max_value=10) # subtract the mean expression value and divide by the standard deviation
-adata
-scanpy.pp.neighbors(adata, n_neighbors=10, n_pcs=40)
-scanpy.tl.umap(adata)
-scanpy.pl.umap(adata, color=['n_genes_by_counts', 'total_counts'])  
